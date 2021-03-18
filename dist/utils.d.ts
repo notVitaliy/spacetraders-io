d67f5c235541bf309755df433842b301df509ead
Vitaliy Isikov <visikov@gmail.com>
2021-03-18 14:48:00 -0400

    Remove dist from git

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { AxiosError } from 'axios';
-export declare const asyncWrap: <T, E = AxiosError<any>>(promise: Promise<T>) => Promise<[E, T]>;
-export declare const asyncSleep: (ms: number) => Promise<unknown>;
--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncSleep = exports.asyncWrap = void 0;
-const asyncWrap = (promise) => new Promise((resolve) => promise.then((data) => resolve([null, data])).catch((err) => resolve([err, null])));
-exports.asyncWrap = asyncWrap;
-const asyncSleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-exports.asyncSleep = asyncSleep;
-//# sourceMappingURL=utils.js.map
